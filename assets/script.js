09f8e0951b5bc27fceaf5974d48233d0260723bb
Jimmys Mac <eadesj25@gmail.com>
2023-03-30 13:04:30 -0400

    added questions, began styling

--- assets/script.js
+++ assets/script.js
@@ -55,53 +55,21 @@ const questions = [
         question: "What does HTML stand for?",
         choices: ["High-Tech Markup Language", "Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Hyper Text Marking Language"],
         answer: "Hyper Text Markup Language",
+    },
+    {
+        question: "Which of the following is NOT a programming language?",
+        choices: ["Javascript", "CSS", "Python", "Ruby"],
+        answer: "CSS",
+    },
+    {
+        question: "What is a variable in programming?",
+        choices: ["A named value that can be changed", "A constant value that cannot be changed", "A function that performs a specific task", "A conditional statement that controls program flow"],
+        answer: "A named value that can be changed",
+    },
+    {
+        question: "Which of the following is NOT a data type in JavaScript?",
+        choices: ["Number", "String", "Boolean", "Float"],
+        answer: "Float",
     }
 ]
-
-// choiceEls.foreach(function(choiceEl) {
-//     choiceEl.addEventListener("click", function () {
-//         if (choiceEl.textContent === currentQuestion.answer) {
-//             console.log("correct");
-//         } else {
-//             console.log("incorrect")
-//         }
-//     })
-// })
-
-// function showScore() {
-//     questionsDiv.innerHTML = "Good score";
-// }
-
-
-// What does HTML stand for?
-// a) High-Tech Markup Language
-// b) Hyper Text Markup Language
-// c) Hyperlinks and Text Markup Language
-// d) Hyper Text Marking Language
-
-// Which of the following is NOT a programming language?
-// a) Java
-// b) CSS
-// c) Python
-// d) Ruby
-
-// What is a variable in programming?
-// a) A named value that can be changed
-// b) A constant value that cannot be changed
-// c) A function that performs a specific task
-// d) A conditional statement that controls program flow
-
-// Which of the following is NOT a data type in JavaScript?
-// a) Number
-// b) String
-// c) Boolean
-// d) Float
-
-// Answers:
-
-// b) Hyper Text Markup Language
-// b) CSS
-// a) A named value that can be changed
-// d) float
-
 
